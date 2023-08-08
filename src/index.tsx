43b07f2d14d214d1912d4385b91b2ba8f763167f
RafDes21 <israeltineo84@gmail.com>
2023-08-08 19:22:23 -0300

    add folder hook/useCalculator

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,9 @@
-import React, { useState } from "react";
 import { View, Text, TouchableHighlight } from "react-native";
 import { styles } from "./styles";
+import { useCalculator } from "./hook/useCalculator";
 
 const App = () => {
-  const [currentCalculation, setCurrentCalculation] = useState<string>("0");
-
-  const handlePress = (value: string) => {
-    if (currentCalculation === "0" || currentCalculation === "Error") {
-      setCurrentCalculation(value);
-    } else {
-      setCurrentCalculation(prevCalculation => prevCalculation + value);
-    }
-  };
-
-  const calculateTotal = () => {
-    try {
-      const total = evaluateExpression(currentCalculation);
-      setCurrentCalculation(total.toString());
-    } catch (error) {
-      setCurrentCalculation("Error");
-    }
-  };
-
-  const onPressClear = () => {
-    setCurrentCalculation("0");
-  };
-
-  const evaluateExpression = (exp: string): number => {
-    const tokens = exp.split(/([+\-x/])/).filter(token => token.trim() !== '');
-  
-    if (tokens.length === 0) {
-      return 0;
-    }
-  
-    let operands: number[] = [];
-    let operators: string[] = [];
-    let currentNumber = parseFloat(tokens[0]);
-  
-    for (let i = 1; i < tokens.length; i += 2) {
-      const operator = tokens[i];
-      const nextNumber = parseFloat(tokens[i + 1]);
-  
-      if (operator === "x" || operator === "/") {
-        if (operator === "x") {
-          currentNumber *= nextNumber;
-        } else {
-          currentNumber /= nextNumber;
-        }
-      } else {
-        operands.push(currentNumber);
-        operators.push(operator);
-        currentNumber = nextNumber;
-      }
-    }
-  
-    operands.push(currentNumber);
-  
-    let result = operands[0];
-  
-    for (let i = 0; i < operators.length; i++) {
-      if (operators[i] === "+") {
-        result += operands[i + 1];
-      } else {
-        result -= operands[i + 1];
-      }
-    }
-  
-    return result;
-  };
+const {currentCalculation, onPressClear,handlePress, calculateTotal} = useCalculator()
   
   return (
     <View style={styles.container}>
