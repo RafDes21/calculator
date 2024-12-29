1d063626685fcaafa7cb9d3e1931a38428a63c17
israe <israeltineo84@gmail.com>
2024-12-29 17:09:16 -0300

    add screen splash

--- app/src/main/java/com/rafdev/calculadora/ui/theme/Color.kt
+++ app/src/main/java/com/rafdev/calculadora/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package com.rafdev.calculadora.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+object CalculatorPalette {
+    val darkBlue = Color(0xFF0F1926)
+    val lightBlue = Color(0xFF2E4159)
+}
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
